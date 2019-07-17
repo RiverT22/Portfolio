828409f14cd1b2fd4edcf53ee5fef8c5870e5776
RiverT22 <bangtan.jungkookie24@gmail.com>
2019-07-17 11:49:50 -0700

    lots of changes

---
+++ practice/number.js
@@ -0,0 +1,15 @@
+// Ask the user for a number
+//var open= prompt ("Type in a number")
+// Convert number type
+openNum=Number(open);
+
+
+//Ask for second number
+//var second= prompt ("Type in another number")
+//Convert second number
+secondNum=Number(second)
+
+// Use a function to calculate the the average of the two numbers
+
+//console.log the average of the two numbers
+console.log     
